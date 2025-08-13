15a87ab2c3d3b2226584dbe1e32e1b0c37e7a6a1
roniahamed <mdroniahamed56@gmail.com>
2025-08-14 04:47:27 +0600

    adding discount amount

--- product/serializers.py
+++ product/serializers.py
@@ -21,11 +21,12 @@ class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     brand = BrandSerializer(read_only=True, allow_null=True)
     tags = TagSerializer(many=True, read_only=True)
+    discounted_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = [
-            'id', 'name', 'description', 'category', 'brand', 'price', 
+            'id', 'name', 'description', 'category', 'brand', 'price','sale_price', 
             'sku', 'stock', 'is_active', 'image', 'weight', 
             'dimensions', 'tags', 'created_at', 'updated_at'
         ]
@@ -41,3 +42,13 @@ class ProductSerializer(serializers.ModelSerializer):
                 message="SKU must be unique."
             )
         ]
+    def get_discounted_price(self, obj):
+
+        if obj.sale_price is not None and obj.price > obj.sale_price:
+            discount_amount = obj.price - obj.sale_price
+            percentage = (discount_amount / obj.price) * 100
+            return {
+                'amount_saved': f"{discount_amount:.2f}",
+                'percentage': f"{percentage:.2f}%",
+            }
+        return None
