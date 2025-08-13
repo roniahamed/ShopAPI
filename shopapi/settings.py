63f15ac92dff5d495644680287eadf25d0b2b19c
roniahamed <mdroniahamed56@gmail.com>
2025-08-13 23:53:01 +0600

    adding app

--- shopapi/settings.py
+++ shopapi/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Rest api
+    'rest_framework',
+    
+    # extra app
+    'product',
 ]
 
 MIDDLEWARE = [
