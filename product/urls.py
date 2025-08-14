ed4d8f215378f470b31042fe0cb8654f3d7b9765
roniahamed <mdroniahamed56@gmail.com>
2025-08-15 01:52:48 +0600

    post method mone api views

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import HomeAPIView
 
 urlpatterns = [
-    
+    path('',HomeAPIView.as_view(), name='home'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response 
 from rest_framework import status 
 from rest_framework.views import APIView
+from .serializers import TagSerializer, BrandSerializer, CategorySerializer, ProductSerializer
 
 class HomeAPIView(APIView):
 
@@ -10,4 +11,15 @@ class HomeAPIView(APIView):
         data = {'message': 'Welcome to ShopAPI!', 'version':'1.0'}
 
         return Response(data, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+
+        received_data = request.data 
+        print(received_data)
         
+        response_data = {
+            'message':'Data received successfully!',
+            'my_data': received_data
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
