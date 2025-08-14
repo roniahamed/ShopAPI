f425b7c2f04ad81c722e8667b179583325f4aa73
roniahamed <mdroniahamed56@gmail.com>
2025-08-15 01:43:31 +0600

    views: HOMEAPIView

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework import status 
+from rest_framework.views import APIView
 
-# Create your views here.
+class HomeAPIView(APIView):
+
+    def get(self, request, format=None):
+
+        data = {'message': 'Welcome to ShopAPI!', 'version':'1.0'}
+
+        return Response(data, status=status.HTTP_200_OK)
+        
