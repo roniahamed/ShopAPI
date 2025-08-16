730d2ee9a6d96c60ab0f89e70669c514d6771a07
roniahamed <mdroniahamed56@gmail.com>
2025-08-16 23:11:20 +0600

    adding unfold

--- shopapi/settings.py
+++ shopapi/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv(
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
