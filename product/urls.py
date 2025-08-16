d5ec335f1ffa1801311738535936eebb91d97de5
roniahamed <mdroniahamed56@gmail.com>
2025-08-17 05:54:48 +0600

    ProductListViewSets api

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
-from .views import HomeAPIView, ProductListAPIView, DetailsProductAPIView, ProductListView, ProductDetailview
+from django.urls import path, include
+from .views import HomeAPIView, ProductListAPIView, DetailsProductAPIView, ProductListView, ProductDetailview, ProductListViewSets
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'products', ProductListViewSets, basename='product')
+
 urlpatterns = [
     path('',HomeAPIView.as_view(), name='home'),
-    path('products/', ProductListAPIView.as_view(), name='products' ),
-    path('products/<int:pk>/', DetailsProductAPIView.as_view(), name='product' ),
+    # path('products/', ProductListAPIView.as_view(), name='products' ),
+    # path('products/<int:pk>/', DetailsProductAPIView.as_view(), name='product' ),
 
-    path('products/generic/', ProductListView.as_view(), name='products-generic' ),
-    path('product/<int:pk>/', ProductDetailview.as_view(), name='product-details' ),
+    # path('products/generic/', ProductListView.as_view(), name='products-generic' ),
+    # path('product/<int:pk>/', ProductDetailview.as_view(), name='product-details' ),
+    path('', include(router.urls)),
 ]
